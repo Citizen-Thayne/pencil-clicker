704ac24c7721f1abe8fe621d689ab4ee4dfe889c
citizenthayne <wiseguy303@gmail.com>
2019-04-24 17:38:12 -0500

    deploy

---
+++ precache-manifest.65cf81d5e5001110ecc985613d5579eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pencil-clicker/robots.txt"
+  },
+  {
+    "revision": "cb06f246cafe1b7f834e",
+    "url": "/pencil-clicker/js/chunk-vendors.695ebca9.js"
+  },
+  {
+    "revision": "c8d446c7c2a1659159ff",
+    "url": "/pencil-clicker/js/app.08315236.js"
+  },
+  {
+    "revision": "caf538caf0c6445e22c4bd4117926c9d",
+    "url": "/pencil-clicker/index.html"
+  },
+  {
+    "revision": "c8d446c7c2a1659159ff",
+    "url": "/pencil-clicker/css/app.b67d96ef.css"
+  }
+];
